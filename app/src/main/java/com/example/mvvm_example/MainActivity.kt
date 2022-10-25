dff8f5f153e63a900b1d03e939e185363b2095d8
HyeIn97 <henna.hyen97@kakaovx.com>
2022-10-25 20:15:00 +0900

    [feat] viewModel 초기화한다.
    뷰모델 초기화하기

--- app/src/main/java/com/example/mvvm_example/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_example/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.mvvm_example
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.mvvm_example.databinding.ActivityMainBinding
+import com.example.mvvm_example.view.NameViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    //private val viewModel: NameViewModel by viewModels()
+    private lateinit var viewModel: NameViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        viewModel = ViewModelProvider(this)[NameViewModel::class.java]
     }
 }
